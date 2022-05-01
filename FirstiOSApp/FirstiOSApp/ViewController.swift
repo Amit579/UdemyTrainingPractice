683cc2deb250e28e85c65391c1d0baf4f079fc50
Amit Sharma <amitsharma5july@gmail.com>
2022-05-01 08:17:55 +0530

    Added uibutton extension and some uicomponents in storyboard

--- FirstiOSApp/FirstiOSApp/ViewController.swift
+++ FirstiOSApp/FirstiOSApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameImageView: UIImageView!
+    @IBOutlet weak var changeNameLbel: UILabel!
+    
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+//MARK:-Change Button Action
+    @IBAction func changeButton(_ sender: UIButton) {
+        changeNameLbel.text = "Hey Hi Amit"
+    }
 }
 
